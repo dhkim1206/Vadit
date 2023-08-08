d795211d50a5a3fa9a8350b50ee5854ad9c7d816
BakSeonhyeong <qkrtjsgud85@gmail.com>
2023-08-08 16:53:57 +0900

    Create SettingData.cs

---
+++ Vadit/Vadit/SettingData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vadit
+{
+    public class SettingData
+    {
+        public enum EnumNotificationLayout
+        {
+            Standard,
+            OnlyUser,
+            Text
+        }
+
+        [Serializable]
+        public class AppConfigClass
+        {
+            public bool Pose;
+            public bool LongPlay;
+            public bool WindowSameExecute;
+            public bool AlarmSound;
+            public int SaveingPeriod;
+            public int CamFrame;
+            public EnumNotificationLayout NotificationLayout;
+
+        }
+    }
+}
